ec9074e9f8e736cb227609109deb1d61f61931bf
XiaoHuizhe <dashi@same.com>
2017-04-05 16:32:29 +0800

    fix

--- src/Response.php
+++ src/Response.php
@@ -6,9 +6,25 @@ namespace Apns2;
  * @see https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/CommunicatingwithAPNs.html#//apple_ref/doc/uid/TP40008194-CH11-SW17
  * @package Apns2
  */
-class Response{
+class Response
+{
     public $device_id;
     public $code;
-    public $status;
+    public $reason;
     public $duration;
+
+    public function __construct($responseHeaderAndBody, $code, $duration)
+    {
+        $this->duration = $duration;
+        $this->code = $code;
+        $m = preg_match('/^\s+ (\d+).*?\\n(.*)$/s', $responseHeaderAndBody);
+        if ($m) {
+            $this->code = $m[1];
+        }
+        $body = $m[2];
+        if ($body) {
+            $json = json_decode($body);
+            $this->reason = isset($json->reason) ? $json->reason : '';
+        }
+    }
 }
