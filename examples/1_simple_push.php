77c7f4878f4ce58b8dc3b01cac58743ac9f74f67
XiaoHuizhe <dashi@same.com>
2017-04-05 16:26:11 +0800

    fix

--- examples/1_simple_push.php
+++ examples/1_simple_push.php
@@ -1,17 +1,26 @@
 <?php
+use Apns2\Connection;
+use Apns2\Message;
+use Apns2\MessageAPSBody;
+use Apns2\Options;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$connection = new \Apns2\Connection(['sandbox' => false, 'cert-path' => '/data/www/v4/worker/cert/http2.pem']);
+$connection = new Connection(['sandbox' => false, 'cert-path' => '/data/www/v4/worker/cert/http2.pem']);
+
+$aps = new MessageAPSBody();
+$aps->alert = 'test 1';
+$aps->sound = 'default';
+
+$message = new Message();
+$message->aps = $aps;
+
+$options = new Options();
+$options->apnsTopic = 'com.ohsame.same2.0';
+
 $responses = $connection->send([
     '81fbf7e296f6c94755832a48476182e4e9586a380116e18a46531b62349504f0',
     'e2d0b464813b6b2371d745dff2b1e5fb6b83b07f7dcd98cc9f1346a7752dcc45'
-], [
-    'aps' => [
-        'alert' => 'test 1',
-        'sound' => 'default'
-    ]
-], [
-    '$apns-topic' => 'com.ohsame.same2.0'
-]);
+], $message, $options);
 $connection->close();
 var_dump($responses);
